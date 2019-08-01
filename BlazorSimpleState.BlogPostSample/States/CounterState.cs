ffa2225b68424f00a2fbd12d0266168945193191
Stavros Kasidis <s.kasidis@epsilonnet.gr>
2019-08-01 13:46:27 +0300

    iteration2-using-state-object

---
+++ BlazorSimpleState.BlogPostSample/States/CounterState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSimpleState.BlogPostSample.States
+{
+    public class CounterState
+    {
+        public int CurrentCount { get; private set; }
+        public void IncrementCounter()
+        {
+            CurrentCount++;
+            OnCountChanged?.Invoke();
+        }
+
+        public void ResetCounter()
+        {
+            CurrentCount = 0;
+            OnCountChanged?.Invoke();
+        }
+
+        public event Action OnCountChanged;
+    }
+}
