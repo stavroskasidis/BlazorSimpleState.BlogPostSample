5f5314c76b8dcb3a1cf7b7cf12896884060e88cf
Stavros Kasidis <s.kasidis@epsilonnet.gr>
2019-08-01 13:54:26 +0300

    iteration-3 with state manager nested class

--- BlazorSimpleState.BlogPostSample/States/CounterState.cs
+++ BlazorSimpleState.BlogPostSample/States/CounterState.cs
@@ -5,21 +5,8 @@ using System.Threading.Tasks;
 
 namespace BlazorSimpleState.BlogPostSample.States
 {
-    public class CounterState
+    public partial class CounterState
     {
         public int CurrentCount { get; private set; }
-        public void IncrementCounter()
-        {
-            CurrentCount++;
-            OnCountChanged?.Invoke();
-        }
-
-        public void ResetCounter()
-        {
-            CurrentCount = 0;
-            OnCountChanged?.Invoke();
-        }
-
-        public event Action OnCountChanged;
     }
 }
---
+++ BlazorSimpleState.BlogPostSample/States/CounterStateManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSimpleState.BlogPostSample.States
+{
+    public partial class CounterState
+    {
+        public class Manager
+        {
+            private readonly CounterState state;
+
+            public Manager(CounterState state)
+            {
+                this.state = state;
+            }
+
+            public void IncrementCounter()
+            {
+                this.state.CurrentCount++;
+                OnCountChanged?.Invoke();
+            }
+
+            public void ResetCounter()
+            {
+                this.state.CurrentCount = 0;
+                OnCountChanged?.Invoke();
+            }
+
+            public event Action OnCountChanged;
+        }
+    }
+}
